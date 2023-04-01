0019d2d7df5d59c7d9e14297198570a3ed82d020
Desislava <desi.peeva@gmail.com>
2023-04-01 21:34:39 +0300

    add more assets

--- library/src/assets/index.ts
+++ library/src/assets/index.ts
@@ -1,9 +1,21 @@
 import logo from "./imgs/logo.svg"
 import logo_white from "./imgs/logo_white.svg"
+import hero from "./imgs/hero.svg"
+import intro_1 from "./imgs/intro_1.svg"
+import intro_2 from "./imgs/intro_2.svg"
+import intro_3 from "./imgs/intro_3.svg"
+import intro_4 from "./imgs/intro_4.svg"
 
 export {
     logo,
-    logo_white
+    logo_white,
+    hero,
+    intro_1,
+    intro_2,
+    intro_3,
+    intro_4
 }
 
 export * as mui from "@mui/material"
+export * as muiIcon from "@mui/icons-material"
+export * as muiLab from "@mui/lab"
