1b19f9c0f5c0c8975e54ae7247c8fec726323f3d
Desislava <desi.peeva@gmail.com>
2023-04-08 00:42:02 +0300

    disable React.StrictMode to avoid double requests

--- library/src/index.tsx
+++ library/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+// import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import "./index.css"
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 )
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    // </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
